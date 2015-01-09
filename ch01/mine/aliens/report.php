de5d31b87b0066fa18726108ea0e7cda0cac8398
Razielek <k.szczegielniak@gmail.com>
2015-01-09 23:27:57 +0100

    First excercises from Ch1. Bookmark 60

---
+++ ch01/mine/aliens/report.php
@@ -0,0 +1,31 @@
+<html>
+
+<head>
+   <title>Aliens Abducted Me - Report an Abduction </title> 
+</head>
+
+<body>
+    <h2> Aliens Abducted Me - Report an Abduction </h2>
+    <?php
+        $name = $_POST['firstname'] . ' ' . $_POST['lastname'];
+        $when_it_happened = $_POST['whenithappened'];
+        $how_long = $_POST['howlong'];
+        $how_many = $_POST['howmany'];
+        $alien_description = $_POST['aliendescription'];
+        $what_they_did = $_POST['whattheydid'];
+        $fang_spotted = $_POST['fangspotted'];
+        $email = $_POST['email'];
+        $other = $_POST['other'];
+
+        echo 'Thanks for submitting the form.</br>';
+        echo 'You were abducted ' . $when_it_happened;
+        echo ' and were gone for ' . $how_long . '</br>';
+        echo 'Number of aliens: ' . $how_many . '</br>';
+        echo 'Describe them: ' . $alien_description . '</br>';
+        echo 'The aliens did this: ' . $what_they_did . '</br>';
+        echo 'Was Fang there? ' . $fang_spotted . '</br>';
+        echo 'Other comments: ' . $other . '</br>';
+        echo 'Your email address is ' . $email;
+    ?>
+</body>
+</html>
