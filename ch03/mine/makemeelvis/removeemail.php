1dbe9bfa6126e4056ed7a5c71d03dfad95a0895c
razielek <k.szczegielniak@gmail.com>
2015-01-11 12:50:41 +0100

    Ch4. Bookmark 193

---
+++ ch03/mine/makemeelvis/removeemail.php
@@ -0,0 +1,15 @@
+<?php
+	$dbc = mysqli_connect('localhost', 'razielek', '10masamune01', 'elvis_store')
+		or die('Error connecting to database');
+
+	$email = $_POST['email'];
+	echo $email;
+	$query = "DELETE FROM email_list WHERE email = '$email'";
+
+	$result = mysqli_query($dbc, $query) or die('Error querying the database');
+
+	echo 'Customer removed: ' . $email;
+
+	mysqli_close($dbc);	
+
+?>
--- ch03/mine/makemeelvis/sendemail.php
+++ ch03/mine/makemeelvis/sendemail.php
@@ -3,20 +3,26 @@
     $subject = $_POST['subject'];
     $text = $_POST['elvismail'];
 
+	if (!empty($subject) && !empty($text))
+	{
+		$dbc = mysqli_connect('localhost', 'razielek', '10masamune01', 'elvis_store')
+			or die('Error connecting the database');
 
-    $dbc = mysqli_connect('localhost', 'razielek', '10masamune01', 'elvis_store')
-        or die('Error connecting the database');
+		$query = "SELECT * FROM email_list";
+		$result = mysqli_query($dbc, $query) or die('Error querying the database');
 
-    $query = "SELECT * FROM email_list";
-    $result = mysqli_query($dbc, $query) or die('Error querying the database');
+		while($row = mysqli_fetch_array($result))
+		{
+			$first_name = $row['first_name'];
+			$last_name = $row['last_name'];
+			$email = $row['email'];
 
-    while($row = mysqli_fetch_array($result))
-    {
-        $first_name = $row['first_name'];
-        $last_name = $row['last_name'];
-        $email = $row['email'];
-
-        echo 'Email sent to ' . $first_name . ' ' . $last_name . ' ' . $email . '</br>';
-    }
-    mysqli_close($dbc);
+			echo 'Email sent to ' . $first_name . ' ' . $last_name . ' ' . $email . '</br>';
+		}
+		mysqli_close($dbc);
+	}
+	else
+	{
+		echo 'Form fields cannot be empty';
+	}
 ?>
